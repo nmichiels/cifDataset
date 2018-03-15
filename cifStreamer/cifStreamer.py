2bb785ed4625ed729c8ca6787b4c406a7dfd5b9a
Nick Michiels <nick.michiels@uhasselt.be>
2018-03-15 09:53:13 +0100

    crop/pad to target resolution. Padding is done by adding random values sampled from normal distribution constructed using image samples

--- cifStreamer/cifStreamer.py
+++ cifStreamer/cifStreamer.py
@@ -11,6 +11,40 @@ import scipy.misc
 
 import h5py
 
+import math
+
+def __pad_or_crop(image, image_size):
+    bigger = max(image.shape[0], image.shape[1], image_size)
+
+    pad_x = float(bigger - image.shape[0])
+    pad_y = float(bigger - image.shape[1])
+
+    pad_width_x = (int(math.floor(pad_x / 2)), int(math.ceil(pad_x / 2)))
+    pad_width_y = (int(math.floor(pad_y / 2)), int(math.ceil(pad_y / 2)))
+    # sample = image[int(image.shape[0]/2)-4:int(image.shape[0]/2)+4, :8]
+    sample = image[-10:,-10:]
+
+    std = np.std(sample)
+
+    mean = np.mean(sample)
+
+    def normal(vector, pad_width, iaxis, kwargs):
+        vector[:pad_width[0]] = np.random.normal(mean, std, vector[:pad_width[0]].shape)
+        vector[-pad_width[1]:] = np.random.normal(mean, std, vector[-pad_width[1]:].shape)
+        return vector
+
+    if (image_size > image.shape[0]) & (image_size > image.shape[1]):
+        return np.pad(image, (pad_width_x, pad_width_y), normal)
+    else:
+        if bigger > image.shape[1]:
+            temp_image = np.pad(image, (pad_width_y), normal)
+        else:
+            if bigger > image.shape[0]:
+                temp_image = np.pad(image, (pad_width_x), normal)
+            else:
+                temp_image = image
+        return temp_image[int((temp_image.shape[0] - image_size)/2):int((temp_image.shape[0] + image_size)/2),int((temp_image.shape[1] - image_size)/2):int((temp_image.shape[1] + image_size)/2)]
+
    
 class CIFDataSet(object):
     
@@ -96,26 +130,35 @@ class HDF5DataSet(object):
 def visualizeCIFDataset(dataset):
     dataset.reset()
 
+    targetSize = 60
     imageCounter = 0
     while (not dataset.eod()):
 
         image, maskImage = dataset.nextImage()
+
+        
         print ("Image " + repr(imageCounter), image.shape)
        
         for channel in range(image.shape[-1]):#range(0,1):#
             img = image[:,:,channel]
             img /= np.amax(img)
             mask = maskImage[:,:,channel]
+
+            img = __pad_or_crop(img, targetSize)
+            mask = __pad_or_crop(mask, targetSize)
+
+
             maxMask = np.amax(mask)
             if (maxMask != 0):
                 mask /= maxMask
             # else:
                 # break
             # scipy.misc.imsave('outfile.jpg', img)
-
+            cv2.imwrite("cell_" + repr(imageCounter) + "_c_" + repr(channel) + ".jpg", img*255)
+            cv2.imwrite("mask_" + repr(imageCounter) + "_c_" + repr(channel) + ".jpg", mask*255)
             cv2.imshow('image',img)
             cv2.imshow('mask',mask)
-            chr = cv2.waitKey(500)
+            chr = cv2.waitKey(20)
         
         imageCounter += 1
         if chr==27: # Esc key to exit
@@ -162,8 +205,8 @@ def convertToHDF5(inputFile, outputFile):
 
     # print(image)
 
-# dataset = CIFDataSet("../05-Aug-2015_A04-noBF.cif")
-dataset = HDF5DataSet("test.dhf5")
+dataset = CIFDataSet("../05-Aug-2015_A04-noBF.cif")
+# dataset = HDF5DataSet("test.dhf5")
 visualizeCIFDataset(dataset)
 # convertToHDF5("../05-Aug-2015_A04-noBF.cif", "test.dhf5")
 # if __name__ == "__main__":
